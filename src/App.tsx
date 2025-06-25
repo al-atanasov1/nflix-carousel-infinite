77f4abaae8f221eb73de63c641980921dd35cf50
Alexander Atanasov <al.atanasov111@gmail.com>
2025-06-25 16:12:49 +0300

    project structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <div className='flex flex-col justify-center items-center w-full h-full'>
+                <h1 className='w-full text-center text-5xl font-bold'>Netflix-like infinite carousel</h1>
+                <div className='alert alert-info text-3xl'>Hello</div>
+            </div>
+        </>
+    );
+}
+
+export default App;
